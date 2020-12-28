f91dc1065b544df4a1938ca3e14652b2c1fe1e8b
Juno Yeon <73125906+kumoy@users.noreply.github.com>
2020-12-28 17:15:40 -0500

    Lints tf files using Terraform v0.14.3

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
-variable valid_items {
+variable "valid_items" {
   description = "List of valid items to validate against"
   type        = list(string)
 }
 
-variable item {
+variable "item" {
   description = "Name of item to validate"
   type        = string
 }
 
-variable name {
+variable "name" {
   description = "Name of the validation test"
   type        = string
 }
